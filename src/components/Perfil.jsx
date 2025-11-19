8fb6e2302badd12d5fb69f590980a4d50450f8d4
david alejandro orozco henao <davidalejandroorozcohenao@gmail.com>
2025-11-19 13:00:35 -0500

    feat: componente Perfil base

---
+++ src/components/Perfil.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Perfil() {
+  return (
+    <section>
+      <h2>Perfil Profesional</h2>
+      <p>
+        Desarrollador  en formación. Experiencia en React, JavaScript y buenas prácticas de desarrollo.
+      </p>
+    </section>
+  );
+}
